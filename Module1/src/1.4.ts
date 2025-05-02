1b8d2fa20beb2c05ead1d5f2ddf8b0e793bf08ba
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-05-02 08:30:51 +0600

    complete primitive data taype learning

---
+++ Module1/src/1.4.ts
@@ -0,0 +1,41 @@
+//Basic Data Type In TypeScript
+
+// 1. Primitives
+// 1. Non Primitives
+
+// List of Primitive Data Types in TypeScript
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Undefind
+// 5.Null
+// 6.Symbol
+// 7.Bigint
+
+
+// NUumber
+const num: number = 10;
+
+// String 
+const str: string = "Hello World";
+
+// Boolean
+const isOpen: boolean = true;
+ 
+// Undifend
+let und: undefined = undefined;
+
+// Null
+let nul: null = null;
+
+
+
+// List of Non Primitive Data Types in TypeScript
+
+// 1.Object
+// 2.Array
+// 3.Function
+
+
+
+
